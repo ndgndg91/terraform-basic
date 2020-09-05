058c95f820cabc8b6178935dcbc59127f76a1feb
ndgndg91 <ndgndg91@gmail.com>
2020-09-06 00:10:05 +0900

    create public subnets

--- vpc_and_ec2/infrastructure/variables.tf
+++ vpc_and_ec2/infrastructure/variables.tf
@@ -7,3 +7,19 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
   description = "VPC cidr"
 }
+
+variable "public_subnet_1_cidr" {
+  description = "Public Subnet 1 CIDR"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "Public Subnet 2 CIDR"
+}
+
+variable "public_subnet_3_cidr" {
+  description = "Public Subnet 3 CIDR"
+}
+
+variable "public_subnet_4_cidr" {
+  description = "Public Subnet 4 CIDR"
+}
--- vpc_and_ec2/infrastructure/vpc.tf
+++ vpc_and_ec2/infrastructure/vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
 terraform {
@@ -7,10 +7,42 @@ terraform {
 }
 
 resource "aws_vpc" "production-vpc" {
-  cidr_block            = "${var.vpc_cidr}"
+  cidr_block            = var.vpc_cidr
   enable_dns_hostnames  = true
 
-  tags {
+  tags = {
     Name = "Production-VPC"
   }
+}
+
+resource "aws_subnet" "public-subnet-1" {
+  cidr_block        = var.public_subnet_1_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  cidr_block        = var.public_subnet_2_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = "ap-northeast-2b"
+
+  tags = {
+    Name = "Public-Subnet-2"
+  }
+}
+
+resource "aws_subnet" "public-subnet-3" {
+  cidr_block        = var.public_subnet_3_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = "ap-northeast-2c"
+}
+
+resource "aws_subnet" "public-subnet-4" {
+  cidr_block        = var.public_subnet_4_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = "ap-northeast-2d"
 }
