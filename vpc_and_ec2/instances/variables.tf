fe0a16754a914fba2a49924b24d5df5f271958f7
ndgndg91 <ndgndg91@gmail.com>
2020-09-06 15:33:01 +0900

    define backend and read remote state for layer 1 infrastructure

---
+++ vpc_and_ec2/instances/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default     = "ap-northeast-2"
+  description = "AWS Region"
+}
+
+variable "remote_state_bucket" {
+  description = "Bucket name for layer 1 remote state"
+}
+
+variable "remote_state_key" {
+  description = "Key name for layer 1 remote state"
+}
