2a4e900777d188c469711c9c5991fc36325b4667
ndgndg91 <ndgndg91@gmail.com>
2020-09-06 17:10:49 +0900

    Create Auto-Scaling Policy for Private EC2 Instances

--- vpc_and_ec2/instances/instances.tf
+++ vpc_and_ec2/instances/instances.tf
@@ -289,8 +289,22 @@ resource "aws_autoscaling_group" "ec2_public_autoscaling_group" {
 }
 
 resource "aws_autoscaling_policy" "webapp_production_scaling_policy" {
-  autoscaling_group_name   = aws_autoscaling_group.ec2_public_autoscaling_group
+  autoscaling_group_name   = aws_autoscaling_group.ec2_public_autoscaling_group.name
   name                     = "Production-WebApp-AutoScaling-Policy"
+  policy_type              = "TargetTrackingPolicy"
+  min_adjustment_magnitude = 1
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 80.0
+  }
+}
+
+resource "aws_autoscaling_policy" "backend-production-scaling-policy" {
+  autoscaling_group_name = aws_autoscaling_group.ec2_private_autoscaling_group.name
+  name                   = "Production-Backend-AutoScaling-Policy"
   policy_type              = "TargetTrackingPolicy"
   min_adjustment_magnitude = 1
 
