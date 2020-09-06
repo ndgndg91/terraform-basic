30cda9284b298c5730f3232f021648eefd1c0ee8
ndgndg91 <ndgndg91@gmail.com>
2020-09-06 16:13:39 +0900

    Launch Configuration for Private EC2 Instances

--- vpc_and_ec2/instances/variables.tf
+++ vpc_and_ec2/instances/variables.tf
@@ -10,3 +10,12 @@ variable "remote_state_bucket" {
 variable "remote_state_key" {
   description = "Key name for layer 1 remote state"
 }
+
+variable "ec2_instances_type" {
+  description = "EC2 Instance type to launch"
+}
+
+variable "key_pair_name" {
+  default     = "terraform-provision"
+  description = "Keypair to use to connect to EC2 Instances"
+}
