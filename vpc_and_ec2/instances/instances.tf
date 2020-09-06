41ff01b371c7df96d055e31a2ba178056e0e3abf
ndgndg91 <ndgndg91@gmail.com>
2020-09-06 15:45:24 +0900

    create security groups for ec2 instances and ELB

--- vpc_and_ec2/instances/instances.tf
+++ vpc_and_ec2/instances/instances.tf
@@ -14,3 +14,79 @@ data "terraform_remote_state" "network_configuration" {
     region  = var.region
   }
 }
+
+resource "aws_security_group" "ec2_public_security_group" {
+  name        = "EC2-Public-SG"
+  description = "Internet reaching access for EC2 Instances"
+  vpc_id      = data.terraform_remote_state.network_configuration.vpc_id
+
+  ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    protocol = "SSH"
+    to_port = 22
+    cidr_blocks = [data.terraform_remote_state.network_configuration.vpc_cidr] //only allow VPN
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1" //everywhere
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_private_security_group" {
+  name = "EC2-Private-SG"
+  description = "Only allow public SG resources to access these instances"
+  vpc_id = data.terraform_remote_state.network_configuration.vpc_id
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = [aws_security_group.ec2_public_security_group.id]
+  }
+
+  ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80 // if using tomcat, change 8080
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow health checking for instances using this SG"
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "elb_security_group" {
+  name        = "ELB-SG"
+  description = "ELB Security Group"
+  vpc_id = data.terraform_remote_state.network_configuration.vpc_id
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow web traffic to load balancer"
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
